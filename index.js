3499d4898a6b2676df7bfdebd886a156f94c7c82
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-02-16 07:10:10 -0600

    Added update function

--- index.js
+++ index.js
@@ -23,4 +23,17 @@ function reset(){
 function update(){
 
     const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
