aa28d5cd4f1ff4d3200a92aa17c338f7f120fe33
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-02-16 07:05:18 -0600

    Created Start function

---
+++ index.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+
+function stop(){
+
+}
+
+function reset(){
+
+}
+
+function update(){
+
+    const currentTime = Date.now();
+}
